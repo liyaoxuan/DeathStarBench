4ab66314992241e88515a92bed8709163b7b12d3
liyaoxuan <yaoxuanli@outlook.com>
2025-04-27 00:25:47 +0800

    test propagate reorder info through carrier

--- socialNetwork/nginx-web-server/lua-scripts/wrk2-api/home-timeline/read.lua
+++ socialNetwork/nginx-web-server/lua-scripts/wrk2-api/home-timeline/read.lua
@@ -82,7 +82,7 @@ function _M.ReadHomeTimeline()
   context["sched-time-next"] = 0
   context["sched-time-remaining"] = 0
   context["sched-time-start"] = math.floor(socket.gettime() * 1000)
-  ngx.log(ngx.INFO, "reschedule: enable=" .. args.enable .. ", sla=" .. args.sla)
+  ngx.log(ngx.ERR, "reschedule: enable=" .. args.enable .. ", sla=" .. args.sla)
   local client = GenericObjectPool:connection(
       HomeTimelineServiceClient, "home-timeline-service" .. k8s_suffix, 9090)
   local status, ret = pcall(client.ReadHomeTimeline, client, req_id,
